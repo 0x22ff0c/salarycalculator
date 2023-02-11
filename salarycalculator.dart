6c3c833ff8a3a7b200dcb1202d9ad70eb79d44d8
0x22ff0c <markandydizon@gmail.com>
2023-02-11 16:05:26 +0800

    Update salarycalculator
    - Add handling for the following events:
    	- when user inputs a blank gross salary
    	- when user inputs a gross salary with characters other than numbers

--- salarycalculator.dart
+++ salarycalculator.dart
@@ -1,4 +1,3 @@
-import 'dart:ffi';
 import 'dart:io';
 
 void main(List<String> args) {
@@ -11,16 +10,38 @@ void main(List<String> args) {
 
   double grossSalaryAmount;
 
+  RegExp regExp = new RegExp("\\D");
+
   if(userGrossSalaryAmount == null || userGrossSalaryAmount == ""){
 
-    print("\nERROR: You did not input any gross salary amount.");
+    print("\n[ERROR] You did not input any gross salary amount.");
+    exit(0);
+
+  }else if (regExp.hasMatch(userGrossSalaryAmount)){
+
+    print("\n[ERROR] Your input contains an invalid numeric character.");
+    print("\n[Error] Your input: " + userGrossSalaryAmount);
     exit(0);
 
   }else{
 
     grossSalaryAmount = double.parse(userGrossSalaryAmount);
-    print("Gross salary: " + grossSalaryAmount.toStringAsPrecision(2));
+    print("Gross salary: " + grossSalaryAmount.toStringAsFixed(2));
 
   }
   
+  // String sample = "10000";
+
+  // RegExp regularExpressions = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+
+  // String Function(Match) matchFunction = (Match match) => '${match[1]},';
+
+  // double sampledouble = double.parse(sample);
+
+  // sample = sampledouble.toStringAsFixed(2);
+
+  // sample = sample.replaceAllMapped(regularExpressions, matchFunction);
+
+  // print(sample);
+
 }
