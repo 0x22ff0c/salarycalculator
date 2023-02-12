ad140845e49cdbbcbb2503508253298a22ccb2fd
0x22ff0c <markandydizon@gmail.com>
2023-02-12 11:09:02 +0800

    Update salary calculator
    - Add logic for computing PAG-IBIG contribution

--- salarycalculator.dart
+++ salarycalculator.dart
@@ -10,7 +10,7 @@ void main(List<String> args) {
 
   double grossSalaryAmount;
 
-  RegExp regExp = new RegExp("\d[^.,]");
+  RegExp regExpUserInput = new RegExp("\d[^.,]");
 
   if(userGrossSalaryAmount == null || userGrossSalaryAmount == ""){
     while(userGrossSalaryAmount == null || userGrossSalaryAmount == ""){
@@ -20,9 +20,9 @@ void main(List<String> args) {
     }
   }
 
-  if(regExp.hasMatch(userGrossSalaryAmount)){
+  if(regExpUserInput.hasMatch(userGrossSalaryAmount)){
     String userInput = userGrossSalaryAmount;
-    while(regExp.hasMatch(userInput)){
+    while(regExpUserInput.hasMatch(userInput)){
       print('\n[ERROR] You provided an invalid amount: $userGrossSalaryAmount');
       stdout.write("\nInput your gross monthly salary here: ");
       userGrossSalaryAmount = stdin.readLineSync();
@@ -30,7 +30,42 @@ void main(List<String> args) {
     }
   }
 
+  RegExp regExpFormattedNumber = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
+  String Function(Match match) matchFunction = (Match match) => '${match[1]},'; 
+
   grossSalaryAmount = double.parse(userGrossSalaryAmount.toString());
-  print("Gross salary: " + grossSalaryAmount.toStringAsFixed(2));
+  
+  userGrossSalaryAmount = grossSalaryAmount.toStringAsFixed(2);
+  
+  userGrossSalaryAmount = userGrossSalaryAmount.toString().replaceAllMapped(regExpFormattedNumber, matchFunction);
+
+  print("Gross salary: " + userGrossSalaryAmount);
+
+  double pagibigDeduction;
+
+
+  if (grossSalaryAmount <= 0.00){
+
+    print("\nNo contribution for: Pag-IBIG");
+  
+  }else if(grossSalaryAmount <= 1500.00){
+
+    pagibigDeduction = grossSalaryAmount * 0.01;
+
+    print("Pag-IBIG contribution: " + pagibigDeduction.toStringAsFixed(2));
+
+  }else if (grossSalaryAmount > 1500.00 && grossSalaryAmount <= 5000.00){
+
+    pagibigDeduction = grossSalaryAmount * 0.02;
+
+    print("Pag-IBIG contribution: " + pagibigDeduction.toStringAsFixed(2));
+
+  }else if (grossSalaryAmount > 5000.00){
+
+    pagibigDeduction = 5000.00 * 0.02;
+
+    print("Pag-IBIG contribution " + pagibigDeduction.toStringAsFixed(2));
+
+  }
   
 }
